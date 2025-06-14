671721498875fa92498f5468e73e50af7ecfe96d
dumbhell <septyanha23@gmail.com>
2025-06-14 09:37:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+function loadContent(section) {
+  const content = document.getElementById("main-content");
+
+  if (section === 'karakter') {
+    content.innerHTML = `
+      <h2>Daftar Karakter</h2>
+      <ul>
+        <li>Ardhan, Sang Penjaga Api</li>
+        <li>Luna, Penyihir Bulan</li>
+        <li>Kael, Ksatria Timur</li>
+      </ul>
+    `;
+  }
+
+  else if (section === 'wilayah') {
+    content.innerHTML = `
+      <h2>Wilayah di Dunia Varethia</h2>
+      <ul>
+        <li>🔺 Gunung Arkael</li>
+        <li>🌊 Lautan Zephira</li>
+        <li>🌳 Hutan Myralen</li>
+      </ul>
+    `;
+  }
+
+  else if (section === 'cerita') {
+    content.innerHTML = `
+      <h2>Cerita Dunia Varethia</h2>
+      <p>Di masa kuno, dunia Varethia terpecah oleh lima elemen...</p>
+    `;
+  }
+}
